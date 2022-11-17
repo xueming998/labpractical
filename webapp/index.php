df9691578a684a37767b34997a27ebdd8367539e
xueming998 <74669530+xueming998@users.noreply.github.com>
2022-11-17 17:19:11 +0800

    xss
    validation

--- webapp/index.php
+++ webapp/index.php
@@ -1,12 +1,26 @@
 <html>
-<head>
-</head>
 <body>
-<form method="post" action="output.php"> 
-	Input <input type="text" name="name" value="">
-	<br><br>
-  <input type="submit" name="submit" value="Submit">  
-</form>
+<script>  
+function validateform(){  
+var name=document.myform.name.value;  
+  
+  var format = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+  if (name.match(format)) {
+  alert("special character");
+  window.location.href = "/index.php";
+  return false;  
+}else {  
+   
+  return true;  
+  }  
+}  
+</script>  
+<body>  
+  <form name="myform" method="post" action="output.php" onsubmit="return validateform()" >  
+search: <input type="text" name="name"><br/>  
+<input type="submit" value="register" >  
+</form>  
 </body>
-
 </html>
+
+ 
